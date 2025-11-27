43e3a596ed3fbadfa6c066ddb1b2000f468ed215
Masaomi Hatakeyama <masaomi.hatakeyama@uzh.ch>
2025-11-27 11:46:45 +0100

    Skip migration check in legacy database mode
    - Disable config.active_record.migration_error when LEGACY_DATABASE=true
    - Prevents PendingMigrationError when connecting to old SUSHI MySQL DB

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -51,7 +51,12 @@ Rails.application.configure do
   config.active_support.disallowed_deprecation_warnings = []
 
   # Raise an error on page load if there are pending migrations.
-  config.active_record.migration_error = :page_load
+  # Skip migration check in legacy database mode (old SUSHI MySQL DB)
+  if ENV['LEGACY_DATABASE'] == 'true'
+    config.active_record.migration_error = false
+  else
+    config.active_record.migration_error = :page_load
+  end
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
